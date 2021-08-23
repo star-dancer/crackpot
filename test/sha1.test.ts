87c47c3d6a9b5b586a29bedcba7a66edac5a3c64
rikka <admin@rikka.cc>
2021-08-23 10:26:45 +0800

    feat: 🎸 add sha1 algo

---
+++ test/sha1.test.ts
@@ -0,0 +1,11 @@
+import { SHA1 } from "../src/algo/sha1";
+import { TestConstant } from "./test.constant";
+
+const testMine = SHA1(TestConstant.TEST_STR).toString();
+
+const trueResult = "dfbceb412adbb530ec841223edc488e8a0f8ea59";
+console.log(testMine);
+console.log(trueResult);
+if (testMine === trueResult) {
+  console.log("success");
+}
