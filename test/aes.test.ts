be1181d34dbdb5a801e7be6c08453e044ce316d2
rikka <admin@rikka.cc>
2021-08-26 19:09:37 +0800

    test: 💍 修改AES测试用例

--- test/aes.test.ts
+++ test/aes.test.ts
@@ -1,7 +1,10 @@
-import { AES } from "../src";
-const key = "123";
+import { Utf8 } from "@/enc/utf8";
 
-const test = AES.encrypt("123", key);
+import { AES } from "../src";
+import { TestConstant } from "./constant/test.constant";
+
+const test = AES.encrypt(TestConstant.TEST_STR, TestConstant.TEST_KEY);
 const result = test.toString();
 console.log(result);
-console.log(AES.decrypt(result, key).toString());
+const ending = AES.decrypt(result, TestConstant.TEST_KEY).toString(Utf8);
+console.log(ending);
